1ce69d3fb88244e9bfba578089bd99bc04dc143d
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-06-29 09:17:33 +0500

    add/** added

--- routes/add/index.js
+++ routes/add/index.js
@@ -116,4 +116,61 @@ Router.post("/bank", (req, res) => {
   }
 });
 
+Router.post("/vehicle", (req, res) => {
+  if (req.body.reg_number) {
+    const { reg_number, active } = req.body;
+
+    const sql = `INSERT INTO cem_vehicle (reg_number,active) VALUES ('${reg_number.toUpperCase()}','${active}')`;
+
+    mysqlConnection.query(sql, (err, result) => {
+      if (err) {
+        const { code, errno } = err;
+        if (code == "ER_DUP_ENTRY" || errno == 1062) {
+          res.json({
+            status: "error",
+            message: "Vehicle already exist",
+            payload: { code, errno },
+          });
+        } else {
+          res.json({ status: "error", message: "Unknown Error", payload: err });
+        }
+      } else {
+        res.json({ status: "success", message: "Vehicle succefully added." });
+      }
+    });
+  } else {
+    res.json({ status: "error", message: "Please enter name of vehicle." });
+  }
+});
+
+Router.post("/code", (req, res) => {
+  if (req.body.code) {
+    const { code, area_name } = req.body;
+
+    const sql = `INSERT INTO cem_codes (code,area_name) VALUES ('${code.toUpperCase()}','${area_name}')`;
+
+    mysqlConnection.query(sql, (err, result) => {
+      if (err) {
+        const { code, errno } = err;
+        if (code == "ER_DUP_ENTRY" || errno == 1062) {
+          res.json({
+            status: "error",
+            message: "Area already exist",
+            payload: { code, errno },
+          });
+        } else {
+          res.json({ status: "error", message: "Unknown Error", payload: err });
+        }
+      } else {
+        res.json({ status: "success", message: "Area succefully added." });
+      }
+    });
+  } else {
+    res.json({
+      status: "error",
+      message: "Please enter code and name of area.",
+    });
+  }
+});
+
 module.exports = Router;
